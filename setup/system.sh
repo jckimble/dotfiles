87cfdc54460f70747d40e8a0d9417d1a28a38c7a
James C Kimble <jckimble601@gmail.com>
2016-12-20 00:57:23 -0600

    gpgcard support

--- setup/system.sh
+++ setup/system.sh
@@ -2,7 +2,7 @@
 HOSTNAME=$1
 pacman -Sy reflector --noconfirm
 reflector -l 5 --sort rate --save /etc/pacman.d/mirrorlist
-pacstrap /mnt base base-devel imagemagick pamixer pulseaudio-alsa pulseaudio-bluetooth bluez bluez-libs bluez-utils bluez-firmware networkmanager networkmanager-openvpn terminus-font alsa-utils scrot xautolock openssh ttf-dejavu linux-headers slim i3 xorg firefox git tmux xclip x11vnc keybase pass gvim grub conky feh pianobar compton eog macchanger mplayer puppet redshift termite weechat wget virtualbox-guest-utils dkms
+pacstrap /mnt base base-devel imagemagick pamixer pulseaudio-alsa pulseaudio-bluetooth ccid bluez bluez-libs bluez-utils bluez-firmware networkmanager networkmanager-openvpn terminus-font alsa-utils scrot xautolock openssh ttf-dejavu linux-headers slim i3 xorg firefox git tmux xclip x11vnc keybase pass gvim grub conky feh pianobar compton eog macchanger mplayer puppet redshift termite weechat wget virtualbox-guest-utils dkms
 genfstab -U -p /mnt >> /mnt/etc/fstab
 arch-chroot /mnt sed -i 's/#en_US.UTF-8/en_US.UTF-8/' /etc/locale.gen
 arch-chroot /mnt locale-gen
@@ -15,6 +15,7 @@ arch-chroot /mnt grub-mkconfig -o /boot/grub/grub.cfg
 arch-chroot /mnt systemctl enable slim
 arch-chroot /mnt systemctl enable NetworkManager
 arch-chroot /mnt systemctl enable bluetooth
+arch-chroot /mnt systemctl enable pcscd
 echo "default_driver=pulse" > /mnt/etc/libao.conf
 arch-chroot /mnt sed -i 's/#default_user/default_user/' /etc/slim.conf
 echo $HOSTNAME > /mnt/etc/hostname
