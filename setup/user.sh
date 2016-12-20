c29023c0e1411240bebcd49120fee486b71e3f0f
James C Kimble <jckimble601@gmail.com>
2016-12-20 01:32:25 -0600

    fix links

--- setup/user.sh
+++ setup/user.sh
@@ -7,10 +7,10 @@ cp -a . /mnt/home/$USER/dotfiles/
 FILES=".bashrc .conkyrc .gitconfig .tmux.conf .tmux.sh .tmux-blue.tmuxtheme .vimrc .xinitrc"
 DIRS=".gnupg .config .m2 .password-store .weechat"
 for file in $FILES; do
-	ln /home/$USER/dotfiles/$file /home/$USER/$file
+	arch-chroot /mnt ln /home/$USER/dotfiles/$file /home/$USER/$file
 done
 for dir in $DIRS; do
-	ln -s /home/$USER/dotfiles/$dir /home/$USER/$dir
+	arch-chroot /mnt ln -s /home/$USER/dotfiles/$dir /home/$USER/$dir
 done
 arch-chroot /mnt chown -R $USER:users /home/$USER
 arch-chroot /mnt chmod -R 700 /home/$USER
