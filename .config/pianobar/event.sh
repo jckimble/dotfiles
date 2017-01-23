7ecd6572f089ee73f57d7a2f79cf5c53c25fc8e4
James C Kimble <jckimble601@gmail.com>
2017-01-22 20:28:25 -0600

    add station

--- .config/pianobar/event.sh
+++ .config/pianobar/event.sh
@@ -1,6 +1,4 @@
 #!/usr/bin/env bash
-
-# create variables
 while read L; do
     k="`echo "$L" | cut -d '=' -f 1`"
     v="`echo "$L" | cut -d '=' -f 2`"
@@ -10,7 +8,7 @@ done < <(grep -e '^\(title\|artist\|album\|stationName\|songStationName\|pRet\|p
 case "$1" in
     songstart)
 		echo -e "$songDuration" > ~/.config/pianobar/songDuration
-		echo -e "$songPlayed" > ~/.config/pianobar/songPlayed
+		echo -e "$songStationName" > ~/.config/pianobar/station
 		echo -e "$album" > ~/.config/pianobar/album
 		echo -e "$artist" > ~/.config/pianobar/artist
 		echo -e "$title" > ~/.config/pianobar/title
