d6cf7ae190350e3acb002f2e90d4b50c483eef3d
James C Kimble <jckimble601@gmail.com>
2017-05-18 22:44:01 -0500

    remove archgpg

--- setup/archgpg/aur.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-cd /tmp
-git clone https://aur.archlinux.org/monkeysign-git.git
-cd monkeysign-git
-makepkg -si --noconfirm
-cd /tmp
-rm -rf monkeysign-git
-cd /
--- setup/archgpg/skel/.config/i3/vbcheck.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-#Check if in virtualbox
-if [ `pacman -Qtq | grep virtualbox-guest-utils | wc -l` -eq 1 ]; then
-	VBoxClient-all
-fi;
