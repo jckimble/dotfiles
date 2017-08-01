fb88dd0e51e59c0e548e32a0bfe959c796d8d977
James C Kimble <jckimble601@gmail.com>
2017-07-31 23:58:19 -0500

    fix run.sh

--- run.sh
+++ run.sh
@@ -2,8 +2,8 @@
 FILES=".bashrc .conkyrc .gitconfig .tmux.conf .tmux.sh .tmux-blue.tmuxtheme .vimrc .xinitrc"
 DIRS=".gnupg .config .m2 .password-store .weechat"
 for file in $FILES; do
-	ln -f $file ../$file
+	ln -f dotfiles/$file ../$file
 done
 for dir in $DIRS; do
-	ln -sf $dir ../$dir
+	ln -sf dotfiles/$dir ../$dir
 done
