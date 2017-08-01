23e26eeb7b0dc916daeea1a156f5f9bc63812e5f
James C Kimble <jckimble601@gmail.com>
2017-07-31 22:12:14 -0500

    update ipw & fix run.sh

--- run.sh
+++ run.sh
@@ -2,8 +2,8 @@
 FILES=".bashrc .conkyrc .gitconfig .tmux.conf .tmux.sh .tmux-blue.tmuxtheme .vimrc .xinitrc"
 DIRS=".gnupg .config .m2 .password-store .weechat"
 for file in $FILES; do
-	ln /home/$USER/dotfiles/$file /home/$USER/$file
+	ln -f $file ../$file
 done
 for dir in $DIRS; do
-	ln -s /home/$USER/dotfiles/$dir /home/$USER/$dir
+	ln -sf $dir ../$dir
 done
