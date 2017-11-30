b1b0e8dbc2da0e6d555367082543d6128157be02
James C Kimble <jckimble601@gmail.com>
2017-11-29 21:35:29 -0600

    update 11:29:2017

--- setup/partition.sh
+++ setup/partition.sh
@@ -20,28 +20,17 @@ n
 p
 3
 
-+8G
+
 t
 3
-82
-n
-p
-4
-
-
-t
-4
 83
 w
 EOF
 mkfs.ext2 ${1}1
 mkfs.ext4 ${1}2
-mkswap ${1}3
-mkfs.ext4 ${1}4
-mount ${1}2 /mnt
+mkfs.ext4 ${1}3
+mount -o noatime,discard ${1}2 /mnt
 mkdir /mnt/boot 
-mount ${1}1 /mnt/boot
-mkswap ${1}3
-swapon ${1}3
+mount -o noatime,discard ${1}1 /mnt/boot
 mkdir /mnt/home
-mount ${1}4 /mnt/home
+mount -o noatime,discard ${1}3 /mnt/home
