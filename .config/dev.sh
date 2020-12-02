4e9b5070ce24332d12f7bbd7dfd228c04f5ca202
James C Kimble <jckimble@pm.me>
2020-12-02 12:44:47 -0600

    12-2-2020.2

---
+++ .config/dev.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+CMD=docker
+if command -v podman &> /dev/null
+then
+	CMD=podman
+fi
+IMAGE=${1:-latest}
+if [ ! -f /tmp/dotfiles.${IMAGE}.updated ]; then
+	$CMD pull registry.gitlab.com/jckimble/dotfiles:${IMAGE}
+	if [ $? -eq 0 ]; then
+		touch /tmp/dotfiles.${IMAGE}.updated
+	fi;
+fi
+$CMD run --rm -it -v /home/jckimble/go/src:/home/jckimble/go/src -v /var/run/user/1000/gnupg:/var/run/user/1000/gnupg registry.gitlab.com/jckimble/dotfiles:${IMAGE}
